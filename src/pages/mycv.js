f7131cb5d939b4925b5572dfa260ca8c377c771e
Phadtharaded (B57) Sawankachirdwilai <phadtharaded-sawan.toon@hotmail.com>
2019-10-14 23:59:24 +0700

    profile

--- src/pages/mycv.js
+++ src/pages/mycv.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import '../styles/style.css'
+import '../styles/style.css';
+import profile from '../img/pic.jpg';
 class FindCV extends React.Component {
   async componentDidMount() {
     // const url = 'http://localhost:3001/api/scg/findingRestaurant';
@@ -11,8 +12,29 @@ class FindCV extends React.Component {
   render() {
     return (
       <div className="maincontrainer">
-        <div>
-          hello my cv
+        <div className="mycv">
+          <div className="topCV">
+            <div className="row">
+              <div className="col-3">
+                <img className="profilepic" src={profile} alt="Profile" />
+                <div className="tagname">MR. PHADTHARADED SAWANKACHIRDWILAI</div>
+              </div>
+              <div className="col-9 info">
+                <h3>Personal Information</h3>
+                <span>Hi my name is Phadtharaded Sawankachirdwilai</span> <br />
+                <span>Age : 24 years old</span><br />
+                <span>Nationality : Thai</span><br />
+                <span>Gender : Male</span><br />
+                <span>Date of birth : 27 October 1995</span><br />
+                <span>Interested in : Software/Application Developer, Machine learning, Deep learning, data analystic, and security</span><br />
+              </div>
+            </div>
+          </div>
+
+          <div className="buttomCV">
+
+          </div>
+
         </div>
       </div>
     )
