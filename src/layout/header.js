1923ab9fc6e4783b02b66cf0da06eef2f7aeb55c
Phadtharaded (B57) Sawankachirdwilai <phadtharaded-sawan.toon@hotmail.com>
2019-10-18 23:20:37 +0700

    adjust header wording

--- src/layout/header.js
+++ src/layout/header.js
@@ -52,7 +52,7 @@ class Header extends React.Component {
   render() {
     return (
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <Link className="navbar-brand" to={'/'} onClick={this.setActiveHome.bind(this)}>SCG - COMPANY</Link>
+        <Link className="navbar-brand" to={'/'} onClick={this.setActiveHome.bind(this)}>SCG - ASSIGNMENT</Link>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
